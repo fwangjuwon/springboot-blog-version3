3c9bdae25832f1617143c04f5b0ae4de880bfb08
fwangjuwon <fwangjuwon@naver.com>
2022-04-20 11:09:01 +0900

    Post 게시글 쓰기 완료 - fileupload 로직추가

--- src/main/java/site/metacoding/blogv3/service/PostService.java
+++ src/main/java/site/metacoding/blogv3/service/PostService.java
@@ -1,7 +1,13 @@
 package site.metacoding.blogv3.service;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,12 +17,17 @@ import site.metacoding.blogv3.domain.category.CategoryRepository;
 import site.metacoding.blogv3.domain.post.Post;
 import site.metacoding.blogv3.domain.post.PostRepository;
 import site.metacoding.blogv3.domain.user.User;
+import site.metacoding.blogv3.handler.ex.CustomException;
+import site.metacoding.blogv3.util.UtilFileUpload;
 import site.metacoding.blogv3.web.dto.post.PostRespDto;
 import site.metacoding.blogv3.web.dto.post.PostWriteReqDto;
 
 @RequiredArgsConstructor
 @Service
 public class PostService {
+
+    @Value("${file.path}")
+    private String uploadFolder;
 
     private final PostRepository postRepository;
     private final CategoryRepository categoryRepository;
@@ -26,22 +37,48 @@ public class PostService {
         return categoryRepository.findByUserId(principal.getId());
     }
 
+    //하나의 서비스는 여러가지 일을 한번에 처리한다(여러가지 일이 하나의 트랜잭션이다)
+    //단점: 여러가지 로직이 하나의 서비스에 공존하고 있다. -> 디버깅 하기 힘듦
     @Transactional
-    public void 게시글쓰기(PostWriteReqDto postWriteReqDto){
+    public void 게시글쓰기(PostWriteReqDto postWriteReqDto, User principal){
 
         //1. 파일 저장 (UUID로 변경해서 저장)
 
-        //2. 이미지 파일명을 Post오브젝트에 thumnail에다가 옮겨야함
+        //(1) UUID로 파일 쓰고 경로 리턴받기 
+       String thumnail = UtilFileUpload.write(uploadFolder, postWriteReqDto.getThumnailFile());
+        // Category category = new Category();
+        // category.set setId(postWriteReqDto.getCategoryId());
 
-        //3. title, content도 Post 오브젝트에 옮기고
-
-        //4. userId 도 Post오브젝트에 옮기기
-
-        //5. categoryId 도 Post오브젝트에 옮기기 
-
-        //save하면 끝 
-
+        //(2) 카테고리 있는지 확인
+    Optional<Category> categoryOp = categoryRepository.findById(postWriteReqDto.getCategoryId());
+        
+    //(3) post DB저장
+    if(categoryOp.isPresent()){
+        Post post = postWriteReqDto.toEntity(thumnail, principal, categoryOp.get());
+        postRepository.save(post);
+    }else{
+throw new CustomException("해당카테고리가 존자해지 않습니다");
     }
+
+
+        // postRepository.mSave(
+        //     postWriteReqDto.getCategoryId(), 
+        //     principal.getId(),
+        //     postWriteReqDto.getTitle(),
+        //     postWriteReqDto.getContent(),
+        //     thumnail);
+
+        // 2. 파일명을 Post 오브젝트에 thumnail로 옮겨야 함
+
+        // 3. title,content도 Post 오브젝트에 옮기고
+
+        // 4. userId도 Post 오브젝트에 옮기기
+
+        // 5. categoryId도 Post 오브젝트에 옮기기
+
+        // 6. save
+    }
+
 
     public PostRespDto 게시글목록보기(int userId) {
         List<Post> postsEntity = postRepository.findByUserId(userId);
--- src/main/java/site/metacoding/blogv3/web/dto/post/PostWriteReqDto.java
+++ src/main/java/site/metacoding/blogv3/web/dto/post/PostWriteReqDto.java
@@ -8,12 +8,18 @@ import org.springframework.web.multipart.MultipartFile;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import site.metacoding.blogv3.domain.category.Category;
+import site.metacoding.blogv3.domain.post.Post;
+import site.metacoding.blogv3.domain.user.User;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class PostWriteReqDto {
     
+    @NotBlank
+    private Integer categoryId;
+
     @Size(min=1, max=60)
     @NotBlank
     private String title;
@@ -21,5 +27,15 @@ public class PostWriteReqDto {
     private MultipartFile thumnailFile; //썸네일은 null허용 
     
     private String content; //null 허용
+
+    public Post toEntity(String thumnail, User principal, Category category){
+        Post post = new Post();
+        post.setTitle(title);
+        post.setContent(content);
+        post.setThumnail(thumnail);
+        post.setUser(principal);
+        post.setCategory(category);
+        return post;
+    }
 
 }
